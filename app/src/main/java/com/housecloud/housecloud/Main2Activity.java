fba4c6a794a31743fb4dd2a5a521cf73ceb9a0c2
jota14 <jozetqp@outlook.es>
2018-01-31 12:35:50 +0100

    prueba 31/01/2018

---
+++ app/src/main/java/com/housecloud/housecloud/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.housecloud.housecloud;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 21609143 on 31/01/2018.
+ */
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
