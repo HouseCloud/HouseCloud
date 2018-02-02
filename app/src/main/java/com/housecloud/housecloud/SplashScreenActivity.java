063ae6b0b7b015b9ac29431dabb1603b749d3c77
jota14 <jozetqp@outlook.es>
2018-02-02 13:38:50 +0100

    Animacion de Splash añadida

--- app/src/main/java/com/housecloud/housecloud/SplashScreenActivity.java
+++ app/src/main/java/com/housecloud/housecloud/SplashScreenActivity.java
@@ -1,17 +1,39 @@
 package com.housecloud.housecloud;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 2000;
+    ImageView img;
+    TextView txvHouse, txvCloud;
+
+    private static int SPLASH_TIME_OUT = 60000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        img = (ImageView)findViewById(R.id.imglogo);
+        txvHouse = (TextView)findViewById(R.id.txvHouse);
+        txvCloud = (TextView)findViewById(R.id.txvCloud);
+
+        Typeface fuente = Typeface.createFromAsset(getAssets(),"DK Crayon Crumble.ttf");
+        Animation translateLogo = AnimationUtils.loadAnimation(this,R.anim.translate_logo);
+        Animation translateHC = AnimationUtils.loadAnimation(this,R.anim.translate_housecloud);
+
+        txvHouse.setTypeface(fuente);
+        txvCloud.setTypeface(fuente);
+        img.startAnimation(translateLogo);
+        txvHouse.startAnimation(translateHC);
+        txvCloud.startAnimation(translateHC);
 
         new Handler().postDelayed(new Runnable() {
             @Override
