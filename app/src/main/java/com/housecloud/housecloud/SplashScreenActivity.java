a6359afc98a005ee72f78bb59805f019fe0ae9a0
jota14 <jozetqp@outlook.es>
2018-02-09 13:00:02 +0100

    Introduccion de Activity Login:  - Login Google  - Login Facebook

--- app/src/main/java/com/housecloud/housecloud/SplashScreenActivity.java
+++ app/src/main/java/com/housecloud/housecloud/SplashScreenActivity.java
@@ -13,32 +13,32 @@ import android.widget.TextView;
 public class SplashScreenActivity extends AppCompatActivity {
 
     ImageView img;
-    TextView txvHouse, txvCloud;
+    //TextView txvHouse, txvCloud;
 
-    private static int SPLASH_TIME_OUT = 60000;
+    private static int SPLASH_TIME_OUT = 2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
         img = (ImageView)findViewById(R.id.imglogo);
-        txvHouse = (TextView)findViewById(R.id.txvHouse);
-        txvCloud = (TextView)findViewById(R.id.txvCloud);
+        //txvHouse = (TextView)findViewById(R.id.txvHouse);
+        //txvCloud = (TextView)findViewById(R.id.txvCloud);
 
-        Typeface fuente = Typeface.createFromAsset(getAssets(),"DK Crayon Crumble.ttf");
+        //Typeface fuente = Typeface.createFromAsset(getAssets(),"KGOneMoreNight.ttf");
         Animation translateLogo = AnimationUtils.loadAnimation(this,R.anim.translate_logo);
-        Animation translateHC = AnimationUtils.loadAnimation(this,R.anim.translate_housecloud);
+        //Animation translateHC = AnimationUtils.loadAnimation(this,R.anim.translate_housecloud);
 
-        txvHouse.setTypeface(fuente);
-        txvCloud.setTypeface(fuente);
+        //txvHouse.setTypeface(fuente);
+        //txvCloud.setTypeface(fuente);
         img.startAnimation(translateLogo);
-        txvHouse.startAnimation(translateHC);
-        txvCloud.startAnimation(translateHC);
+        //txvHouse.startAnimation(translateHC);
+        //txvCloud.startAnimation(translateHC);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this,MainActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this,LoginActivity.class);
                 startActivity(i);
             }
         },SPLASH_TIME_OUT);
